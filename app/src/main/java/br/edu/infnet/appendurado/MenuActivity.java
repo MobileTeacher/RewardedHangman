d8cc460c7a85271c4af9c25b8b9c2bc66aa13beb
hallison.paz <hallison.paz@prof.infnet.edu.br>
2018-09-13 08:16:55 -0300

    testing flavors

--- app/src/main/java/br/edu/infnet/appendurado/MenuActivity.java
+++ app/src/main/java/br/edu/infnet/appendurado/MenuActivity.java
@@ -5,14 +5,56 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+import static com.google.android.gms.ads.AdRequest.ERROR_CODE_INTERNAL_ERROR;
+import static com.google.android.gms.ads.AdRequest.ERROR_CODE_INVALID_REQUEST;
+import static com.google.android.gms.ads.AdRequest.ERROR_CODE_NETWORK_ERROR;
+import static com.google.android.gms.ads.AdRequest.ERROR_CODE_NO_FILL;
 
 public class MenuActivity extends AppCompatActivity {
 
 
+    AdView mAdView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        MobileAds.initialize(this,
+                "ca-app-pub-5837960030724452~1935876669");
+
+        mAdView = findViewById(R.id.adView);
+
+        mAdView.setAdListener(new AdListener(){
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+                TextView textView = findViewById(R.id.debug_text);
+                switch (errorCode){
+                    case ERROR_CODE_INTERNAL_ERROR:
+                        textView.setText("ERROR_CODE_INTERNAL_ERROR");
+                        break;
+                    case ERROR_CODE_INVALID_REQUEST:
+                        textView.setText("ERROR_CODE_INVALID_REQUEST");
+                        break;
+                    case ERROR_CODE_NETWORK_ERROR:
+                        textView.setText("ERROR_CODE_NETWORK_ERROR");
+                        break;
+                    case ERROR_CODE_NO_FILL:
+                        textView.setText("ERROR_CODE_NO_FILL");
+                        break;
+                }
+            }
+        });
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 
     public void startGame(View botao){
