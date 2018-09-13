57a835903d4e3cd2293192830f5fa12bd4317588
hallison.paz <hallison.paz@prof.infnet.edu.br>
2018-09-13 19:36:32 -0300

    added flavor during class

---
+++ app/src/full/java/br/edu/infnet/appendurado/full/MenuDemoActivity.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appendurado.full;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_demo);
+    }
+}
